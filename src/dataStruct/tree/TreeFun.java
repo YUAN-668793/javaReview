40f6b336c386e494bee4596a94210fc449431e30
JIAYUAN <2352840798@qq.com>
2018-08-18 16:21:45 +0800

    二叉树 前序遍历

---
+++ src/dataStruct/tree/TreeFun.java
@@ -0,0 +1,18 @@
+package dataStruct.tree;
+
+/**
+ * @author Jiayuan
+ * @version 1.0
+ * @description:
+ * @time 2018/8/18 13:24
+ */
+public class TreeFun {
+
+    public void preOrder(BinaryTreeNode root){
+        if(root != null){
+            System.out.println(root.getData());
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+        }
+    }
+}
