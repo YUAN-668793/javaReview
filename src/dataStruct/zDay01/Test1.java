dbf731f6387122991dcfe01b5d71ae03fb9ac936
JIAYUAN <2352840798@qq.com>
2018-08-22 15:19:01 +0800

    基于数组实现栈(复习)

--- src/dataStruct/zDay01/Test1.java
+++ src/dataStruct/zDay01/Test1.java
@@ -1,43 +1,39 @@
-package dataStruct.stack;
+package dataStruct.zDay01;
 
 /** 基于数组实现栈
  * @author Jiayuan
  * @version 1.0
  * @description:
- * @time 2018/8/17 20:36
+ * @time 8/22/2018 3:09 PM
  */
-public class ArrayStack {
+public class Test1 {
     private int top;
     private int capacity;
     private int[] array;
-    public ArrayStack(){
+    public Test1(){
+        top = -1;
         capacity = 1;
         array = new int[capacity];
-        top = -1;
     }
     public boolean isEmpty(){
         return top == -1;
     }
     public boolean isStackFull(){
-        return (top == capacity-1);
+        return top == capacity-1;
     }
     public void push(int data){
         if(isStackFull()){
-            System.out.println("Stack Overflow");
+            System.out.println("栈满了");
         }else{
             array[++top] = data;
         }
     }
     public int pop(){
         if(isEmpty()){
-            System.out.println("Stack isEmpty");
+            System.out.println("栈为空");
             return 0;
         }else{
             return array[top--];
         }
-    }
-
-    public void deleteStack(){
-        top = -1;
     }
 }
