456c1ed59324440e357c57fb57fd6aa2f94d2e66
JIAYUAN <2352840798@qq.com>
2018-08-27 09:52:17 +0800

    无序线性查找

---
+++ src/search/UnsortedSearch.java
@@ -0,0 +1,25 @@
+package search;
+
+/** 无序线性查找
+ * @author Jiayuan
+ * @version 1.0
+ * @description:
+ * @time 8/27/2018 9:48 AM
+ */
+public class UnsortedSearch {
+    public int unsorteddLinearSearch(int[] a,int data){
+        for(int i=0;i<a.length;i++){
+            if(a[i] == data){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,0,2,7,8,5,3,6};
+        UnsortedSearch us = new UnsortedSearch();
+        int result = us.unsorteddLinearSearch(a,5);
+        System.out.println(result);
+    }
+}
