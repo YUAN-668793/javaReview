0a2b6a678175686e34e1336e188918990893e907
JIAYUAN <2352840798@qq.com>
2018-08-27 15:00:08 +0800

    二叉搜索树非递归版本

---
+++ src/dataStruct/tree/BinarySearchTreeNode1.java
@@ -0,0 +1,23 @@
+package dataStruct.tree;
+
+/** 二叉搜索树非递归版本
+ * @author Jiayuan
+ * @version 1.0
+ * @description:
+ * @time 8/27/2018 2:54 PM
+ */
+public class BinarySearchTreeNode1 {
+    public BinaryTreeNode find(BinaryTreeNode root,int data){
+        if(root == null) return null;
+        while(root != null){
+            if(data == root.getData()){
+                return root;
+            }else if(data>root.getData()){
+                root = root.getRight();
+            }else{
+                root =root.getLeft();
+            }
+        }
+        return null;
+    }
+}
