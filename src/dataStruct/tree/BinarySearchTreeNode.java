7734e2676b61beb36affde57ba050353a1f1f19a
JIAYUAN <2352840798@qq.com>
2018-08-27 15:10:19 +0800

    二叉搜索树搜索最大/最小元素

--- src/dataStruct/tree/BinarySearchTreeNode.java
+++ src/dataStruct/tree/BinarySearchTreeNode.java
@@ -8,6 +8,8 @@ package dataStruct.tree;
  * @time 8/27/2018 2:48 PM
  */
 public class BinarySearchTreeNode {
+
+    //搜索指定元素
     public BinaryTreeNode find(BinaryTreeNode root,int data){
         if(root == null){
             return null;
@@ -17,5 +19,31 @@ public class BinarySearchTreeNode {
             return find(root.getRight(),data);
         }
         return root;
+    }
+
+    //搜索最小元素
+    public BinaryTreeNode findMin(BinaryTreeNode root){
+        if(root == null){
+            return null;
+        }else{
+            if(root.getLeft() == null){
+                return root;
+            }else{
+                return findMin(root.getLeft());
+            }
+        }
+    }
+
+    //搜索最大元素
+    public BinaryTreeNode findMax(BinaryTreeNode root){
+        if(root == null){
+            return null;
+        }else{
+            if(root.getRight() == null){
+                return root;
+            }else{
+                return findMax(root.getRight());
+            }
+        }
     }
 }
