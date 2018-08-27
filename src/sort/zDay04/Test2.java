da4c69b3efee0e2f75ebd5965823b0ffce1b3c77
JIAYUAN <2352840798@qq.com>
2018-08-27 21:03:35 +0800

    选择排序（zDay4)

--- src/sort/zDay04/Test2.java
+++ src/sort/zDay04/Test2.java
@@ -9,6 +9,25 @@ package sort.zDay04;
 public class Test2 {
     public void selectSort(int[] a){
         int len = a.length;
+        for(int i=0;i<len;i++){
+            int min = a[i];
+            int position = 0;
+            for(int j = i;j<len;j++){
+                if(a[j]<min){
+                    min = a[j];
+                    a[j] = a[i];
+                    a[i] = min;
+                }
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        Test2 t = new Test2();
+        int[] a = {1,0,5,2,3,7,8,9,6};
+        t.selectSort(a);
+        for(int num:a){
+            System.out.print(num);
+        }
     }
 }
