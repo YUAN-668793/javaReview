578957a8f966fc986e3f892868d0f59cc8190b1d
JIAYUAN <2352840798@qq.com>
2018-09-06 11:03:20 +0800

    循环队列

--- src/qiuzhao/date20180906/dataStruct/queue/ArrayQueue.java
+++ src/qiuzhao/date20180906/dataStruct/queue/ArrayQueue.java
@@ -1,40 +1,33 @@
-package dataStruct.queue;
+package qiuzhao.date20180906.dataStruct.queue;
 
-/** 基于简单循环数组实现队列
+/** 循环队列
  * @author Jiayuan
  * @version 1.0
  * @description:
- * @time 2018/8/18 10:10
+ * @time 9/6/2018 10:42 AM
  */
 public class ArrayQueue {
+    private int rear ;
     private int front;
-    private int rear;
     private int capacity;
     private int[] array;
-    private ArrayQueue(int size){
+
+    public ArrayQueue(int size){
         capacity = size;
         front = -1;
         rear = -1;
         array = new int[capacity];
     }
 
-    public static ArrayQueue createQueue(int size){
-        return new ArrayQueue(size);
-    }
     public boolean isEmpty(){
         return front == -1;
     }
     public boolean isFull(){
-        return ((rear+1)%capacity == front);
+        return (rear+1)%capacity == front;
     }
-    public int getQueueSize(){
-        return ((capacity-front+rear+1)%capacity);
-    }
-
-    //在队列的队尾插入一个元素
     public void enQueue(int data){
         if(isFull()){
-            System.out.println("Queue overflow");
+            System.out.println("queue is full");
         }else{
             rear = (rear+1)%capacity;
             array[rear] = data;
@@ -43,21 +36,18 @@ public class ArrayQueue {
             }
         }
     }
-
-    //删除并返回队首的元素
     public int deQueue(){
         int data = 0;
         if(isEmpty()){
             System.out.println("Queue Empty");
         }else{
             data = array[front];
-                if(front == rear) {
-                    front = rear - 1;
-                }else{
-                    front = (front+1)%capacity;
-                }
+            if(front == rear) {
+                front = rear - 1;
+            }else{
+                front = (front+1)%capacity;
+            }
         }
         return data;
     }
-
 }
