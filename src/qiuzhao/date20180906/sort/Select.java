0720e45d10efe7ae1d57164d636f184894740478
JIAYUAN <2352840798@qq.com>
2018-09-06 12:39:27 +0800

    选择排序

---
+++ src/qiuzhao/date20180906/sort/Select.java
@@ -0,0 +1,36 @@
+package qiuzhao.date20180906.sort;
+
+/** 选择排序
+ * @author Jiayuan
+ * @version 1.0
+ * @description:
+ * @time 9/6/2018 12:33 PM
+ */
+public class Select {
+    public void selectSort(int[] a){
+        int len = a.length;
+        int min;
+        int position;
+        for(int i=0;i<len;i++){
+            min = a[i];
+            position = i;
+            for(int j=i;j<len;j++){
+                if(min>a[j]){
+                    min = a[j];
+                    position = j;
+                    a[position] = a[i];
+                    a[i] = min;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,0,3,6,5,3,7,8,9,4};
+        Select se = new Select();
+        se.selectSort(a);
+        for(int num:a){
+            System.out.print(num);
+        }
+    }
+}
