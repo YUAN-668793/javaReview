91a02130bcc618e742405876ce777955d50e787a
JIAYUAN <2352840798@qq.com>
2018-09-17 22:50:46 +0800

    2018/9/17

---
+++ src/test/myTest/Test12.java
@@ -0,0 +1,16 @@
+package test.myTest;
+
+/**
+ * @author Jiayuan
+ * @version 1.0
+ * @description:
+ * @time 9/15/2018 6:53 PM
+ */
+public class Test12 {
+    public void test(int a,int b){
+        System.out.println(a+b);
+    }
+    public int test(int a,int b,int c){
+        return a+b+c;
+    }
+}
